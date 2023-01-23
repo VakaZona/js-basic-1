2c34059e4b008747141e8d39ed70ad77c4600008
Valery Zonov <valery.zonov@sibers.com>
2023-01-23 15:37:33 +0700

    operator ??

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-console.log('Vasy' || 'Oleg')
-console.log(false || 'Oleg')
-
-console.log('Vasy' && 'Oleg')
-console.log(false && 'Oleg')
-console.log(false && false)
-
-let a 
-let b = 'Masha'
-const username = a || 'Petr'
-const username2 = b || 'Petr'
-console.log(username)
-console.log(username2)
- 
-const isAdmin = true
-const fileName = isAdmin && 'file.mp4'
-console.log(fileName)
+let userName = 'Vasy'
+console.log(userName || 'Default username')
+userName = 0
+console.log(userName ?? 'Default username')
