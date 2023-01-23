3542099236d7be06e180c95faecd45b0cc38553e
Valery Zonov <valery.zonov@sibers.com>
2023-01-23 13:26:07 +0700

    Practice 2 (if else)

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const money = 100
+const startDeposit = 12000
+const procent = 0.07
+const timeMouth = 24
 
-if (money > 50) {
-    console.log('Can buy item')
-} else if (money > 5) {
-    console.log('Can buy mini item')
+const endDeposit = startDeposit * (1 + procent / 12) ** timeMouth
+console.log(`Deposit End ${endDeposit}`)
+
+if (endDeposit > 13500) {
+    console.log('Yes')
 } else {
-    console.log('No money')
+    console.log('No')
 }
+
