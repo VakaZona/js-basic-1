93925b6216ea82dd0bcb507049c2b3097ea084b7
Valery Zonov <valery.zonov@sibers.com>
2023-01-23 14:00:36 +0700

    Ternary operators

--- app.js
+++ app.js
@@ -1,42 +1,18 @@
+const bmwX3Price = 100000
+const forFocusPrice = 10000
+const money = 20000
 
-const role = 'manager'
+let message
 
-switch (role) {
-    case 'manager':
-        console.log('Manager')
-        break
-    case 'admin':
-        console.log('Admin')
-        break
-    case 'ceo':
-        console.log('CEO')
-        break
-    default:
-        console.log('Anonymys')
+if (money > bmwX3Price) {
+    message = 'BMW X3'
+} else {
+    message = 'Bicycle'
 }
 
-switch (role) {
-    case 'manager':
-    case 'admin':
-        console.log('No CEO')
-        break
-    case 'ceo':
-        console.log('CEO')
-        break
-    default:
-        console.log('Anonymys')
-}
+ const str = money > bmwX3Price ? 'BMW X3' : 'Bicycle'
 
-const num = 1
-
-switch (true) {
-    case num > 0:
-        console.log('+')
-        break
-    case num < 0:
-        console.log('-')
-        break
-    case num === 0:
-        console.log('0')
-        break
-}
+console.log(`I can buy ${message}`)
+console.log(`I can buy ${str}`) 
+console.log(`I can buy ${money > bmwX3Price ? 'BMW X3' : 'Bicycle'}`)
+console.log(`I can buy ${money > bmwX3Price ? 'BMW X3' : money > forFocusPrice ? 'Ford Focus' : 'Bicycle'}`)
