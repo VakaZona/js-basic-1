a2dc5a1adc052b7df078173acbe97504685be98e
Valery Zonov <valery.zonov@sibers.com>
2023-01-23 12:19:39 +0700

    Practice 1

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
-let a = 5;
-let b = 5.6;
-
-console.log(typeof a);
-console.log(typeof b);
-
-a = 'string';
-console.log(typeof a);
-
-let isAdmin = a > 10;
-console.log(typeof isAdmin);
-
-let c;
-console.log(typeof c);
-c = 5;
-console.log(typeof c);
-
-let d = null;
-console.log(d == null);
-console.log(typeof d);
+const payRaitUSD = 80;
+const projectHours = 40;
+const availableHours = (11 - 2) * 5;
+console.log('Going to Work ' + (availableHours > projectHours));
+console.log('Pay work ' + (projectHours * payRaitUSD) + ' USD')
