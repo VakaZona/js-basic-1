f7c187d8b91029ab52acaf7b891557290a28e458
Valery Zonov <valery.zonov@sibers.com>
2023-01-23 12:48:35 +0700

    False in type

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-const age = '18';
-console.log(age + 5);
-console.log(Number(age) + 5)
-console.log(age - 5)
-console.log(Number(age) - 5)
-
-const userName = 'Vasy'
-console.log(Number(userName) + 5)
-
-console.log(typeof NaN)
-
-console.log(String(4) + 7)
-
-console.log(Boolean(2))
+console.log(Boolean(0))
 console.log(Boolean(''))
-console.log(Boolean('ABCD'))
-
-const a = 2 + '10' //string
-console.log(a - 10) //number
+let a
+console.log(Boolean(a))
+console.log(Boolean(null))
+console.log(Boolean(Number('xs')))
