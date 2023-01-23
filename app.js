18af1c344aa2d4a3c47cf38dcb2742359c2ec371
Valery Zonov <valery.zonov@sibers.com>
2023-01-23 12:33:28 +0700

    template string

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const payRaitUSD = 80;
-const projectHours = 40;
-const availableHours = (11 - 2) * 5;
-console.log('Going to Work ' + (availableHours > projectHours));
-console.log('Pay work ' + (projectHours * payRaitUSD) + ' USD')
+const projectName = 'Site Shop';
+const price = 2000;
+const author = 'Vasy Pupkin'
+
+const template = 'Author ' + author + ' Oreder ' + projectName + ' price ' + price
+console.log(template)
+
+const template2 = `Author ${author} order ${projectName} price ${price}`
+console.log(template2)
+
+const template3 = 'Project ' + projectName + '\n' + 'Price: ' + price
+console.log(template3) 
+
+const template4 = `Project ${projectName}
+Price ${price}`
+console.log(template4)
