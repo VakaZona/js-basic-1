a03326e1499566ca993595185aa16b58de097493
Valery Zonov <valery.zonov@sibers.com>
2023-01-23 15:47:47 +0700

    Practice 4 (logics operators)

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-let userName = 'Vasy'
-console.log(userName || 'Default username')
-userName = 0
-console.log(userName ?? 'Default username')
+const balance = 1001
+const bonusBalance = 99
+const isBanned = false
+const isExist = false
+const isSelling = true
+
+const result = isSelling && !isBanned && !isExist && (balance > 1000 || bonusBalance > 100)
+console.log(`Can buy? ${result ? 'Yes' : 'No'}`)
