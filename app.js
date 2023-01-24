0eed68cf2c31ac95755708cdc47cde6fc51bc231
Valery Zonov <valery.zonov@sibers.com>
2023-01-24 18:21:27 +0700

    aonym function

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-function logName(name, surname) {
-    console.log(`My name ${name} ${surname}`)
+function powerOfTwo(num) {
+    return num * num
 }
 
-logName('Anton', 'Antonov')
+console.log(powerOfTwo(5))
 
-function countDepositSum(depositInUSD, month, rate) {
-    const sum = depositInUSD * (1 + rate / 12) ** month
-    return sum
-}
-
-const example1 = countDepositSum(1000, 24, 0.12)
-console.log(example1)
-
-const example2 = countDepositSum(1000, 48, 0.10)
-console.log(example2)
+const poft = function(num) {
+    return num * num
+} 
+console.log(poft(6))
