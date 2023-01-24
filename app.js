5ef9e21af71fdc9b96d2a823a762717fed4f78ad
Valery Zonov <valery.zonov@sibers.com>
2023-01-24 18:31:08 +0700

    => function

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 function powerOfTwo(num) {
+    console.log(num)
     return num * num
 }
-
 console.log(powerOfTwo(5))
 
-const poft = function(num) {
-    return num * num
-} 
+const poft = (num) => num * num
 console.log(poft(6))
+
+const poft2 = num => {
+    console.log(num)
+    return num * num
+}
+console.log(poft2(7))
