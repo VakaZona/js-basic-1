21ca9c379ad83313dc84302f98569e60fc0b815c
Valery Zonov <valery.zonov@sibers.com>
2023-01-25 17:42:23 +0700

    while

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-// for (let i = 1; i < 5; i++) {
-//     console.log(`Cycle 1 - ${i}`)
-//     for (let j = 1; j < 5; j++){
-//         console.log(`Cycle 2 - ${j}`)
-//     }
-// }
-
-const tasks = [[1, 'Task 1'], [2, 'Task 2']]
-
-for (let i = 0; i < tasks.length; i++) {
-    for (let j = 0; j < tasks[i].length; j++){
-        console.log(tasks[i][j])
-    }
+let i = 1
+while (i < 5) {
+    console.log(i)
+    i++
 }
+
+const arr = [1, 4, 8, 7]
+let j = 0
+while (arr[j] <= 5 && j < arr.length) {
+    console.log(arr[j])
+    j++
+}
+
+let c = 0;
+do {
+    console.log(c)
+    c++
+} while (c < 0) 
