4eb80967946b7c837c3b8e118f688872972b583b
Valery Zonov <valery.zonov@sibers.com>
2023-01-25 17:05:52 +0700

    Practice for

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-const tasks = ['Task 1', 'Task 2', 'Task 3', 'Task 4']
+const arr = ['!', 'JS', 'love', 'I']
+let res = []
 
-for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i] === 'Task 2'){
-        continue
-    }
-    console.log(tasks[i])
+for (i = 0; i < arr.length; i++) {
+    res = res.concat(arr[arr.length - 1 - i])
+    console.log(arr[arr.length - 1 - i])
 }
 
-console.log('-----------')
+console.log(res.join(' '))
 
-for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i] === 'Task 3'){
-        break
-    }
-    console.log(tasks[i])
+let res2 = []
+for (let i = arr.length - 1; i >= 0; i--) {
+    res2.push(arr[i])
 }
-
-               
+console.log(res2.join(' '))
