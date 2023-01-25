d357823c64c6247eb6b0f0af234e98d744b1b888
Valery Zonov <valery.zonov@sibers.com>
2023-01-25 16:06:31 +0700

    contunie and break

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-for (let i = 1; i < 10; i++) {
-    console.log(`Balance ${i}$`)
+const tasks = ['Task 1', 'Task 2', 'Task 3', 'Task 4']
+
+for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i] === 'Task 2'){
+        continue
+    }
+    console.log(tasks[i])
 }
+
+console.log('-----------')
+
+for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i] === 'Task 3'){
+        break
+    }
+    console.log(tasks[i])
+}
+
+               
