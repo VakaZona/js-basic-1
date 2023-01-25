e7249784b68b9b1fe4c2c52debb6c86c7c98e32d
Valery Zonov <valery.zonov@sibers.com>
2023-01-25 14:41:57 +0700

    Practice array

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-const roles = ['user', 'admin', 'manager', 'superuser']
+const tasks = ['Task 1']
 
-const url = '/auth/user/login'
+function addTask (task, tasks) {
+    tasks.push(task)
+    console.log(tasks)
+}
 
-const res = url.split('/')
-console.log(res)
+function deleteTask(task, tasks) {
+    const elementId = tasks.indexOf(task)
+    if (elementId === -1) {
+        return
+    }
+    tasks.splice(elementId, 1)
+    console.log(tasks)
 
-console.log(roles.join('-'))
+}
+
+function unshiftTask(task, tasks) {
+    const elementId = tasks.indexOf(task)
+    if (elementId === -1) {
+        return
+    }
+    const deleteTask = tasks.splice(elementId, elementId)
+    newTask = deleteTask.join('')
+    tasks.unshift(newTask)
+    console.log(tasks)
+}
+
+addTask('Task 2', tasks)
+addTask('Task 3', tasks)
+addTask('Task 4', tasks)
+deleteTask('Task 3', tasks)
+unshiftTask('Task 4', tasks)
+
+
+
