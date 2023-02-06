711187b78269bacda6f6680fca95cb83dfef56a8
Valery Zonov <valery.zonov@sibers.com>
2023-02-06 14:59:22 +0700

    Practice method string

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-const userName = 'Vasy Pupkin'
+const fullUserName = 'Vasy aka Terminator Pupkin'
 
+const userName = fullUserName.slice(0, fullUserName.indexOf(' '))
 console.log(userName)
-console.log(userName[0])
-console.log(userName.charAt(2))
-
-console.log(userName.length)
-
-console.log(userName.indexOf('P'))
-console.log(userName.lastIndexOf('p'))
-console.log(userName.indexOf('pk'))
-console.log(userName.includes('pk'))
-
-console.log(userName.slice(5))
-console.log(userName.slice(5,7))
+const userSurname = fullUserName
+    .slice(fullUserName.lastIndexOf(' ') + 1, fullUserName.length)
+console.log(userSurname)
