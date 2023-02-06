d97c86bc765eb6c0f56b1f7bcda20857205ff0a1
Valery Zonov <valery.zonov@sibers.com>
2023-02-06 14:15:54 +0700

    practice some

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 const arr = [2, 4, 5, 10, 20]
 
-let elGT5
-elGT5 = arr.find(el => el > 5)
-console.log(elGT5)
-
-elGT5Index = arr.findIndex(el => el > 5)
-console.log(elGT5Index)
+console.log(arr.some(el => el === 0))
+console.log(arr.some(el => el === 10))
