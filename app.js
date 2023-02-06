92eb192c138f0f8524b282747023f4e492326c35
Valery Zonov <valery.zonov@sibers.com>
2023-02-06 13:29:31 +0700

    filter

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const transactionInUSD = [10, -7, 50, -10, 100]
+const operations = [100, -20, 7, -20, 50]
 
-const transactionInRUB = transactionInUSD.map((transaction, i) => {
-    console.log(i)
-    return transaction * 60
-})
+const positiveOperations = operations.filter(
+    operation => {
+        return operation > 0
+    }
+)
+console.log(positiveOperations)
 
-console.log(transactionInRUB)
-console.log(transactionInUSD)
+const positiveRUBOperations = operations.filter(
+    operation => {
+        return operation > 0
+    }
+).map(operation => operation * 60)
+console.log(positiveRUBOperations)
