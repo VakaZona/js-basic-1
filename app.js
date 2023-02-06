caa40bb674a1612c754c0888999d6e1bc8743325
Valery Zonov <valery.zonov@sibers.com>
2023-02-06 13:38:36 +0700

    practice map and filter

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-const operations = [100, -20, 7, -20, 50]
+const prices = [[100, 200], [120, 100], [200, 350]]
 
-const positiveOperations = operations.filter(
-    operation => {
-        return operation > 0
-    }
-)
-console.log(positiveOperations)
-
-const positiveRUBOperations = operations.filter(
-    operation => {
-        return operation > 0
-    }
-).map(operation => operation * 60)
-console.log(positiveRUBOperations)
+const result = prices
+    .map(product => product[1] - product[0])
+    .filter(price => price > 0)
+console.log(result)
