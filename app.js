76ef10f7cf60e3b5c8a800fc114feb982f116e37
Valery Zonov <valery.zonov@sibers.com>
2023-02-06 15:59:56 +0700

    practice check string

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
-const str = 'Vasy Pupkina'
-console.log(str.includes('a'))
-console.log(str.startsWith('V'))
-console.log(str.endsWith('n'))
+const num1 = '89103235356'
+const num2 = '+70567654323'
+const num3 = '+7(910) 323-53-56'
+const num4 = '+7(910)2344543'
+const num5 = '   +7(910) 323-53-56'
 
-console.log(new String('Vasy Pupkin'))
+const num1Error = '89959442'
+const num2Error = '+7dewf9959442'
+const num3Error = '988+70953'
+const num4Error =  '8910954d542'
 
-console.log(str.toLowerCase())
-console.log(str.toUpperCase())
-console.log(str.replace('V', 'W'))
-console.log(str.replaceAll('a', 'W'))
-console.log(str.replace(/a/g, 'i'))
+ function isPhoneNumber(num) {
+    num = num.trim()
+    num = num.replace('+7', '8')
+    if (!num.startsWith('8')) {
+        return false
+    }
+    num = num.replaceAll('(', '')
+    num = num.replaceAll(')', '')
+    num = num.replaceAll(' ', '')
+    num = num.replaceAll('-', '')
+    if (num.length !=11) {
+        return false
+    }
+    onlyNumber = true
+    for (const char of num) {
+        if (isNaN(Number(char))) {
+            onlyNumber = false
+            break
+        }
+    }
+    return onlyNumber
+ }
 
-const str2 = '   VAsfger    GErbrs'
-console.log(str2)
-console.log(str2.trim())
+console.log(isPhoneNumber(num1))
+console.log(isPhoneNumber(num2))
+console.log(isPhoneNumber(num3))
+console.log(isPhoneNumber(num4))
+console.log(isPhoneNumber(num5))
+
+console.log(isPhoneNumber(num1Error))
+console.log(isPhoneNumber(num2Error))
+console.log(isPhoneNumber(num3Error))
+console.log(isPhoneNumber(num4Error8))
