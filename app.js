f14fd8fb8663628cf018c823de48a14ec58a6845
Valery Zonov <valery.zonov@sibers.com>
2023-02-06 13:22:36 +0700

    map

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const score = [5, 10, 0, 15]
+const transactionInUSD = [10, -7, 50, -10, 100]
 
-// for (const [i, el] of score.entries()) {
-//     console.log(`Round ${i+1}:  ${el}`)
-// }
-
-
-
-score.forEach((el, i) => {
-    console.log(`Round ${i+1}: ${el}`)
+const transactionInRUB = transactionInUSD.map((transaction, i) => {
+    console.log(i)
+    return transaction * 60
 })
+
+console.log(transactionInRUB)
+console.log(transactionInUSD)
