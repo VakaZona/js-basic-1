04ff7b6732205c060076d01f0bd12ed164a80082
Valery Zonov <valery.zonov@sibers.com>
2023-02-06 12:43:18 +0700

    callback

--- app.js
+++ app.js
@@ -1,46 +1,19 @@
-const operations = [1000, -700, 300, -500, 10000]
-const balance = 100
-
-function AvarageOperations (opertaions) {
-    let income = 0
-    let countIncome = 0
-    let expenses = 0
-    let countExpenses = 0
-    for (let item of operations) {
-        if (item >= 0) {
-            income += item
-            countIncome ++
-        } else {
-            expenses += item
-            countExpenses --
-        }
-        
-    }
-    console.log(`Avarage income ${income / countIncome}`)
-    console.log(`Avarage expenses ${expenses / countExpenses}`)
+function add(a, b) {
+    return a + b
 }
 
-function CheckOperation(balance, operation) {
-    if (balance <= operation) {
-        return true
-    } else {
-        return false
-    }
+function subtract(a, b) {
+    return a - b
 }
 
-function FinishBalance (balance, operations) {
-    let finishBalance = balance
-    for (let item of operations) {
-        finishBalance += item
-        if (CheckOperation(finishBalance, item)) {
-            return console.log('operation false')
-            break
-        }
-        
-    }
-    return finishBalance
+function calculate(a, b, fn) {
+    console.log(fn.name)
+    const res = fn(a, b)
+    return res
 }
 
-FinishBalance(balance, operations)
-console.log(`Finish balance: ${FinishBalance(balance, operations)}`)
-AvarageOperations(operations)
+const res = calculate(1, 2, add)
+console.log(res)
+
+const res2 = calculate(4, 2, subtract)
+console.log(res2)
