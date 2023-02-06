c815c20200542d36863f600e4f4378d62000eaff
Valery Zonov <valery.zonov@sibers.com>
2023-02-06 15:45:23 +0700

    method string

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const fullUserName = 'Vasy aka Terminator Pupkin'
+const str = 'Vasy Pupkina'
+console.log(str.includes('a'))
+console.log(str.startsWith('V'))
+console.log(str.endsWith('n'))
 
-const userName = fullUserName.slice(0, fullUserName.indexOf(' '))
-console.log(userName)
-const userSurname = fullUserName
-    .slice(fullUserName.lastIndexOf(' ') + 1, fullUserName.length)
-console.log(userSurname)
+console.log(new String('Vasy Pupkin'))
+
+console.log(str.toLowerCase())
+console.log(str.toUpperCase())
+console.log(str.replace('V', 'W'))
+console.log(str.replaceAll('a', 'W'))
+console.log(str.replace(/a/g, 'i'))
+
+const str2 = '   VAsfger    GErbrs'
+console.log(str2)
+console.log(str2.trim())
