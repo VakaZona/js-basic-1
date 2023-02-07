1f279dde04d17f3c29400d52156692275f04e0d7
Valery Zonov <valery.zonov@sibers.com>
2023-02-07 13:01:48 +0700

    oprional chaining

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-let user = {
-    name: 'vasy',
-    age: 40,
-    city: 'Moscow'
+const cities = {
+    msk: {
+        temp: {
+            celcius: 25
+        }
+    },
+    spb: {
+
+    }
 }
 
-const {age, ...userWithoutAge} = user
-console.log(age)
-console.log(userWithoutAge)
-
-const additionalData = {
-    skills: ['Dev', 'Designer'],
-    creditCard: '2345-2345-2345-2345'
+const city = 'spb'
+if (cities[city] != undefined && cities[city].temp != undefined) {
+    console.log(cities[city].temp.celcius)
 }
 
-// user.skills = additionalData.skills
-// user.creditCard = additionalData.creditCard
-// console.log(user)
-
-user = {
-    ...user,
-    ...additionalData,
-}
-console.log(user)
+console.log(cities[city]?.temp?.celcius)
