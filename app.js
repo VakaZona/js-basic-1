fd0a55cf2da854816197049d74e6ed38e6da4ef6
Valery Zonov <valery.zonov@sibers.com>
2023-02-07 12:07:40 +0700

    sort object

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
-const userArray = ['Vasy', 'Pupkin', 24]
-const user = {
-    name: 'Vasy',
-    surname: 'Pupkin',
-    age: 24,
-    skills: [
-        'Programming',
-        'Cooking'
-    ],
-    eduBasic: 'School 10',
-    eduPro: 'MIT'
-}
+const users = [
+    {name: 'Vasy', age: 30},
+    {name: 'Kate', age: 18},
+    {name: 'Anna', age: 40},
+    {name: 'Piter', age: 25},
+]
 
-console.log(user)
-console.log(user.name)
-console.log(user.skills[0])
-
-console.log(user['skills'])
-const level = 'Pro'
-console.log(user['edu' + level])
-
-const res = prompt('Input ')
-console.log(user[res])
-
-user['city'] = 'Moscow'
-console.log(user)
-
-user.age = 30
-console.log(user)
+console.log(users)
+console.log(users.sort((a, b) => {
+    return a.age - b.age
+}))
