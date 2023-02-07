6827fc80ba538ad639657c251faf5973582c076d
Valery Zonov <valery.zonov@sibers.com>
2023-02-07 12:05:15 +0700

    elements object

--- app.js
+++ app.js
@@ -7,4 +7,23 @@ const user = {
         'Programming',
         'Cooking'
     ],
+    eduBasic: 'School 10',
+    eduPro: 'MIT'
 }
+
+console.log(user)
+console.log(user.name)
+console.log(user.skills[0])
+
+console.log(user['skills'])
+const level = 'Pro'
+console.log(user['edu' + level])
+
+const res = prompt('Input ')
+console.log(user[res])
+
+user['city'] = 'Moscow'
+console.log(user)
+
+user.age = 30
+console.log(user)
