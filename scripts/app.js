27d2699ca9fc7b16324cb6554332fded114fa27d
Valery Zonov <valery.zonov@sibers.com>
2023-02-09 16:54:36 +0700

    fix delete

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
 
 let habbits = []
 const HABBIT_KEY = 'HABBIT_KEY'
-let globaActiveHabbitId
+let globalActiveHabbitId
 
 
 //page
@@ -45,11 +45,40 @@ function saveData() {
     localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits))
 }
 
+function resetForm(form, fields) {
+    for(const field of fields) {
+        form[field].value = ''
+    }
+}
+
+function validateAndGetFormData(form, fields) {
+    const formData = new FormData(form)
+    const res = {}
+    for(const field of fields) {
+        const fieldValue = formData.get(field)
+        form[field].classList.remove('error')
+        if (!field) {
+            form[field].classList.add('error')
+        }
+        res[field] = fieldValue
+    }
+    let isValid = true
+    for(const field of fields) {
+        if (!res[field]){
+            isValid = false
+        }
+    }
+    if(!isValid) {
+        return 
+    }
+    return res
+}
+
 //Render
 function rerenderMenu(activeHabbit) {
     
     for (const habbit of habbits) {
-        const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`)
+        const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
         if (!existed) {
             //create
             const element = document.createElement('button')
@@ -94,7 +123,7 @@ function rerenderContent(activeHabbit) {
 }
 
 function rerender(activeHabbitId) {
-    globaActiveHabbitId = activeHabbitId
+    globalActiveHabbitId = activeHabbitId
     const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId)
     if (!activeHabbit) {
         return
@@ -105,43 +134,40 @@ function rerender(activeHabbitId) {
 }
 //work with days
 function addDays(event) {
-    const form = event.target
     event.preventDefault()
-    const data = new FormData(event.target)
-    const comment = data.get('comment')
-    form['comment'].classList.remove('error')
-    if (!comment) {
-        form['comment'].classList.add('error')
+    const data = validateAndGetFormData(event.target, ['comment'])
+    if (!data) {
+        return
     }
     habbits = habbits.map(habbit => {
-        if (habbit.id === globaActiveHabbitId) {
+        if (habbit.id === globalActiveHabbitId) {
             return {
                 ...habbit,
-                days: habbit.days.concat([{comment}])
+                days: habbit.days.concat([{comment: data.comment}])
             }
         }
         return habbit
     })
-    
-    form['comment'].value = ''
-    rerender(globaActiveHabbitId)
+    resetForm(event.target, ['comment'])
+    rerender(globalActiveHabbitId)
     saveData()
 }
 
 function deleteDay(index) {
-    habbits = habbits.map(habbit => {
-        if(habbit.id === globaActiveHabbitId) {
-            habbit.days.splice(index, 1)
-            return {
-                ...habbit,
-                days: habbit.days
-            }
-        }
-        return
-    })
-    rerender(globaActiveHabbitId)
-    saveData()
-}
+ 	habbits = habbits.map(habbit => {
+ 		if (habbit.id === globalActiveHabbitId) {
+ 			habbit.days.splice(index, 1);
+ 			return {
+ 				...habbit,
+ 				days: habbit.days
+ 			};
+ 		}
+ 		return habbit;
+ 	});
+ 	rerender(globalActiveHabbitId);
+ 	saveData();
+ }
+
 
 //Working with habbits 
 function setIcon(context, icon) {
@@ -152,6 +178,24 @@ function setIcon(context, icon) {
     
 }
 
+function addHabbit(event) {
+    event.preventDefault()
+    const data = validateAndGetFormData(event.target, ['name', 'icon', 'target'])
+    if (!data) {
+        return
+    }
+    const maxId = habbits.reduce((acc, habbit) => acc > habbit.id ? acc : habbit.id, 0)
+    habbits.push({
+        id: maxId + 1,
+        name: data.name,
+        target: data.target,
+        icon: data.icon,
+        days: []
+    })
+    resetForm(event.target, ['name', 'target'])
+    saveData()
+    rerender(maxId + 1)
+}
 
 //init
 (() => {
